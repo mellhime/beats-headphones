62f22a55e9bebd0a8c8ba3b4e7811dd32c7f7aa9
Daria Bondarenko <mell_hime@mail.ru>
2024-10-28 09:58:36 +0300

    Add for colors section

--- beats-headphones/index.js
+++ beats-headphones/index.js
@@ -1,30 +1,27 @@
 console.log('hi')
 
-// function toggleEmployeeInfo(elem) {
-//     let liElem = elem.closest('li')
-//     if (liElem.classList.contains('team__employee--active')) {
-//         liElem.classList.remove('team__employee--active');
-//     } else {
-//         liElem.classList.add('team__employee--active');
-//     }
-// }
+const teamNamesList = document.getElementsByClassName('team__name');
 
-const teamNamesList = document.getElementsByClassName('team__name'); // HTML collection
-const teamNamesListArray = [...teamNamesList]; // Array
-const teamNamesListArray2 = Array.from(teamNamesList); // Array
-
-console.log(teamNamesList);
-console.log(teamNamesListArray);
-console.log(teamNamesListArray2);
-
-teamNamesListArray.forEach((el) => {
-    console.log(el);
-    el.addEventListener("click", function (elem) {
-        let liElem = elem.closest('li')
+[...teamNamesList].forEach((el) => {
+    el.addEventListener("click", function () {
+        let liElem = el.closest('li')
         if (liElem.classList.contains('team__employee--active')) {
             liElem.classList.remove('team__employee--active');
         } else {
             liElem.classList.add('team__employee--active');
+        }
+    });
+});
+
+const colorsList = document.getElementsByClassName('colors__item');
+
+[...colorsList].forEach((el) => {
+    el.addEventListener("click", function () {
+        let liElem = el
+        if (liElem.classList.contains('colors__item--active')) {
+            liElem.classList.remove('colors__item--active');
+        } else {
+            liElem.classList.add('colors__item--active');
         }
     });
 });
