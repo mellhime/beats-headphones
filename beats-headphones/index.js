48ab30c1514da51a5d7549ddefb3ea68dfba2178
Daria <mell_hime@mail.ru>
2024-11-02 13:12:57 +0300

    Add listeners to fields (#7)
    * Add listeners to for fields in colors and team sections

---
+++ beats-headphones/index.js
@@ -0,0 +1,27 @@
+console.log('hi')
+
+const teamNamesList = document.getElementsByClassName('team__name');
+
+[...teamNamesList].forEach((el) => {
+    el.addEventListener("click", function () {
+        let liElem = el.closest('li')
+        if (liElem.classList.contains('team__employee--active')) {
+            liElem.classList.remove('team__employee--active');
+        } else {
+            liElem.classList.add('team__employee--active');
+        }
+    });
+});
+
+const colorsList = document.getElementsByClassName('colors__item');
+
+[...colorsList].forEach((el) => {
+    el.addEventListener("click", function () {
+        let liElem = el
+        if (liElem.classList.contains('colors__item--active')) {
+            liElem.classList.remove('colors__item--active');
+        } else {
+            liElem.classList.add('colors__item--active');
+        }
+    });
+});
