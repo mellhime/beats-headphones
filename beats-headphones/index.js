09db148167ff55094ea3f800bf57d7b795898af6
Daria <mell_hime@mail.ru>
2024-12-11 15:25:54 +0300

    Add slider to purchase section and some imrpovements (#9)
    * Add slider to purchase section and some imrpovements

--- beats-headphones/index.js
+++ beats-headphones/index.js
@@ -1,27 +1,17 @@
-console.log('hi')
+import colorsListener from './javascript/_colors_section.js';
+import burgerNavigationListener from "./javascript/_info_section.js";
+import reviewsListener from "./javascript/_reviews_section.js";
+import teamDescriptionListener from "./javascript/_team_section.js";
+import { scrollListener } from "./javascript/_common.js";
 
-const teamNamesList = document.getElementsByClassName('team__name');
+colorsListener()
+burgerNavigationListener()
+reviewsListener()
+teamDescriptionListener()
+scrollListener()
 
-[...teamNamesList].forEach((el) => {
-    el.addEventListener("click", function () {
-        let liElem = el.closest('li')
-        if (liElem.classList.contains('team__employee--active')) {
-            liElem.classList.remove('team__employee--active');
-        } else {
-            liElem.classList.add('team__employee--active');
-        }
-    });
-});
+const glide = new Glide('.glide', {
+    perView: 1
+})
 
-const colorsList = document.getElementsByClassName('colors__item');
-
-[...colorsList].forEach((el) => {
-    el.addEventListener("click", function () {
-        let liElem = el
-        if (liElem.classList.contains('colors__item--active')) {
-            liElem.classList.remove('colors__item--active');
-        } else {
-            liElem.classList.add('colors__item--active');
-        }
-    });
-});
+glide.mount().update()
