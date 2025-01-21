1eda4dea5ed9c133e2d4d07b6f471935171bb802
Daria <mell_hime@mail.ru>
2025-01-21 15:31:50 +0300

    Add project building with gulp (#10)
    * Add project building with gulp
    
    ---------
    
    Co-authored-by: ivan.ego <ivan.ego@cadolabs.io>

---
+++ src/javascript/team_section.js
@@ -0,0 +1,17 @@
+export default function teamDescriptionListener () {
+    const teamNamesList = document.getElementsByClassName('team__name');
+
+    [...teamNamesList].forEach((el) => {
+        el.addEventListener("click", function () {
+            let liElem = el.closest('li')
+            if (liElem.classList.contains('team__employee--active')) {
+                liElem.classList.remove('team__employee--active');
+            } else {
+                [...teamNamesList].forEach((el) => {
+                    el.closest('li').classList.remove('team__employee--active');
+                })
+                liElem.classList.add('team__employee--active');
+            }
+        });
+    });
+}
